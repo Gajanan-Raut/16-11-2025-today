26969d20554c798a626a4ba2702a2357d14c8520
Gajanan <gajananraut142@gmail.com>
2025-11-15 15:29:58 +0530

    this is first commit

---
+++ python.py
@@ -0,0 +1,7 @@
+age = int(input('Enter your are'))
+if age > 18 :
+	print('your are not eligible to vote write now : ')
+	a = age - 18 
+	print( 'you are eligible to vote after {a} year 		later )
+else :
+	print("you are able to take vote " )
